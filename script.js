e43c98e8f0d5bbe640fe27776f63b330df228b92
NoorAlJirdabi <noor.aljirdabi@gmail.com>
2024-09-08 15:32:55 +0300

    create functions for slide and combine and check if game over

--- script.js
+++ script.js
@@ -94,6 +94,30 @@ document.addEventListener('DOMContentLoaded', () => {
     return row
   }
 
+  const slideAndCombine = (row) => {
+    let slideRow = slide(row)
+    let combineRow = combine(slideRow)
+    return slide(combineRow)
+  }
+
+  const checkGameOver = () => {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (grid[i][j] === 0) {
+          return false
+        }
+        if (i < 3 && grid[i][j] === grid[i + 1][j]) {
+          return false
+        }
+        if (j < 3 && grid[i][j] === grid[i][j + 1]) {
+          return false
+        }
+      }
+    }
+
+    return true
+  }
+
   newButton.addEventListener('click', startGame)
 
   startGame()
